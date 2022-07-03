4aaa1b21a135f10af9ff27464d1a86fc8396a6ba
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-07-03 12:33:43 +0430

    Search bar bug fixed.

--- JS/users.js
+++ JS/users.js
@@ -8,6 +8,32 @@ searchBtn.onclick = ()=>{
     searchBtn.classList.toggle("active");
 }
 
+// Adding functionality to the search bar
+searchBar.onkeyup = () => {
+    let searchTerm = searchBar.value;
+
+    if (searchTerm != "") {
+        searchBar.classList.add("active");
+    } else {
+        searchBar.classList.remove("active")
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/search.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+            let data = xhr.response;
+            usersList.innerHTML = data;
+          }
+      }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send('searchTerm=' + searchTerm);
+}
+
+
+
 // check every half a second
 setInterval( () => {
     let xhr = new XMLHttpRequest();
@@ -16,7 +42,9 @@ setInterval( () => {
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
               let data = xhr.response;
-              usersList.innerHTML = data;
+              if (!searchBar.classList.contains("active")) {
+                usersList.innerHTML = data;
+              }
           }
       }
     }
--- php/users.php
+++ php/users.php
@@ -8,19 +8,8 @@
 
     if (mysqli_num_rows($sql) == 1) {
         $output .= "No users are available to chat.";
-    } else {
-        while ($row = mysqli_fetch_assoc($sql)) {
-            $output .= '<a href="#">
-                        <div class="content">
-                            <img src="./php/images/'. $row["img"] .'" alt="">
-                            <div class="details">
-                                <span>' . $row["fname"] . " " . $row["lname"] .'</span>
-                                <p>'. $row["status"] .'</p>
-                            </div>
-                        </div>
-                        <div class="status-dot"><i class="fas fa-circle"></i></div>
-                        </a>';
-        }
+    } elseif (mysqli_num_rows($sql) > 1){
+        include "data.php";
     }
 
     echo $output
