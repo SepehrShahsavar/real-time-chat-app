bd6f276a306d2ef74020c953098fb9507155ffe9
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-07-03 16:05:21 +0430

    offline-online status completed. Project Completed.

--- php/data.php
+++ php/data.php
@@ -1,5 +1,7 @@
 <?php
     while ($row = mysqli_fetch_assoc($sql)) {
+        ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
+
         $output .= '<a href="chat.php?user_id='.$row["unique_id"].'">
                     <div class="content">
                         <img src="./php/images/'. $row["img"] .'" alt="">
@@ -8,7 +10,7 @@
                             <p>'. $row["status"] .'</p>
                         </div>
                     </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                    <div class="status-dot '. $offline .'"><i class="fas fa-circle"></i></div>
                     </a>';
     }
 
