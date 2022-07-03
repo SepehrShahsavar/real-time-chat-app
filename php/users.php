2f7fb3abaec7177432818678699c288ec1bf71a4
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-07-03 15:46:30 +0430

    Sending messages complited.

--- php/users.php
+++ php/users.php
@@ -1,16 +1,16 @@
 <?php
     session_start();
     include "config.php";
-
-    $sql = mysqli_query($conn, "SELECT * FROM users");
+    $unique_id = $_SESSION["unique_id"];
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id != {$unique_id}");
     
     $output = "";
 
-    if (mysqli_num_rows($sql) == 1) {
+    if (mysqli_num_rows($sql) == 0) {
         $output .= "No users are available to chat.";
-    } elseif (mysqli_num_rows($sql) > 1){
+    } elseif (mysqli_num_rows($sql) > 0){
         include "data.php";
     }
 
-    echo $output
+    echo $output;
 ?>
