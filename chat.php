94a26fef38b052435a4f88108de352576ed2411b
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-07-03 14:21:35 +0430

    connecting chat page to each user

--- chat.php
+++ chat.php
@@ -1,15 +1,29 @@
 <?php
+    session_start();
+    if (!isset($_SESSION['unique_id'])){
+        header("Location : login.php");
+    }
+?>
+<?php
     include_once "header.php";
 ?>
     <body>
         <div class="wrapper">
           <section class="chat-area">
             <header>
-                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-                <img src="profile.jpg" alt="">
+                <?php
+                    include_once "php/config.php";
+                    $user_id = mysqli_real_escape_string($conn, $_GET["user_id"]);
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+                    if (mysqli_num_rows($sql) > 0) {
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                ?>
+                <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="./php/images/<?php echo $row["img"]; ?>" alt="">
                     <div class="details">
-                        <span>Sepehr Shahsavar</span>
-                        <p>Active now</p>
+                        <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+                        <p><?php echo $row['status'] ?></p>
                     </div>
             </header>
             <div class="chat-box">
@@ -19,7 +33,7 @@
                     </div>
                 </div>
                 <div class="chat incoming">
-                    <img src="profile.jpg" alt="">
+                    <img src="./php/images/<?php echo $row["img"]; ?>" alt="">
                     <div class="details">
                         <p>Lorem ipsum dolor sit amet, consectetour</p>
                     </div>
@@ -30,7 +44,7 @@
                     </div>
                 </div>
                 <div class="chat incoming">
-                    <img src="profile.jpg" alt="">
+                    <img src="./php/images/<?php echo $row["img"]; ?>" alt="">
                     <div class="details">
                         <p>Lorem ipsum dolor sit amet, consectetour</p>
                     </div>
@@ -41,7 +55,7 @@
                     </div>
                 </div>
                 <div class="chat incoming">
-                    <img src="profile.jpg" alt="">
+                    <img src="./php/images/<?php echo $row["img"]; ?>" alt="">
                     <div class="details">
                         <p>Lorem ipsum dolor sit amet, consectetour</p>
                     </div>
